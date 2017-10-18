9cde560e10e89c439c6f7624e4f93eb13a0975ff
RayDragon <govind.resarch0000@gmail.com>
2017-10-19 01:56:01 +0530

    first commit

---
+++ audio.py
@@ -0,0 +1,40 @@
+import speech_recognition as sr
+from gtts import gTTS as gts
+import os
+import pygame
+
+r = sr.Recognizer()
+m = sr.Microphone()
+pygame.init()
+pygame.mixer.init()
+
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to ", r.energy_threshold)
+    while True:
+        print("Ready to record....:")
+        with m as source: audio = r.listen(source)
+        print("Let me recognise it :) ")
+        try:
+            value = r.recognize_google(audio)
+            tts = gts(text='you said,' + value, lang='en')
+            filename = 'temp.mp3'
+            tts.save(filename)
+
+            pygame.mixer.music.load(filename)
+            pygame.mixer.music.play()
+            print(value)
+            while pygame.mixer.music.get_busy():
+                pygame.time.Clock().tick(10)
+            os.remove(filename)  # remove temperory file
+            if value == 'exit' or value == 'Exit':
+                break
+
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+
+except KeyboardInterrupt:
+    pass
